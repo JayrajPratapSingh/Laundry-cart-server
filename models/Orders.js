b24eb51edda59fea359ee13c7f8b05bbc6df5be7
JayrajPratapSingh <jairaj273310@gmail.com>
2023-01-19 14:26:03 +0530

    uploading server side of Laundry cart app

---
+++ models/Orders.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const ordersSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "laundrycartUsers",
+  },
+  totalQuantity: Number,
+  totalPrice: Number,
+  location: String,
+  Address: String,
+  Shirts: {
+    name: String,
+    washtype: String,
+    quantity: Number,
+    totalPrice: Number,
+  },
+  Tshirts: {
+    name: String,
+    washtype: String,
+    quantity: Number,
+    totalPrice: Number,
+  },
+  Trousers: {
+    name: String,
+    washtype: String,
+    quantity: Number,
+    totalPrice: Number,
+  },
+  Jeans: {
+    name: String,
+    washtype: String,
+    quantity: Number,
+    totalPrice: Number,
+  },
+  Boxers: {
+    name: String,
+    washtype: String,
+    quantity: Number,
+    totalPrice: Number,
+  },
+  Joggers: {
+    name: String,
+    washtype: String,
+    quantity: Number,
+    totalPrice: Number,
+  },
+});
+
+module.exports = mongoose.model("orders", ordersSchema);
